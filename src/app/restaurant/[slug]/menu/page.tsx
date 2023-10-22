73ba21eb185e951f8cf56e05f83fcc86f6633738
Paulo Amorim <phdja25@gmail.com>
2023-10-22 20:33:47 -0300

    refactor all the pages into components

--- src/app/restaurant/[slug]/menu/page.tsx
+++ src/app/restaurant/[slug]/menu/page.tsx
@@ -1,5 +1,8 @@
 import NavBar from "@/app/components/NavBar";
 import Link from "next/link";
+import Header from "../components/Header";
+import RestaurantNavBar from "../components/RestaurantNavBar";
+import Menu from "../components/Menu";
 
 export default function MenuPage() {
   return (
@@ -7,45 +10,17 @@ export default function MenuPage() {
       <main className="max-w-screen-2xl m-auto bg-white">
         {/* NAVBAR */}
        <NavBar />
-        {/* NAVBAR */} {/* HEADER */}
-        <div className="h-96 overflow-hidden">
-          <div className="bg-center bg-gradient-to-r from-[#0f1f47] to-[#5f6984] h-full flex justify-center items-center">
-            <h1 className="text-7xl text-white captitalize text-shadow text-center">
-              Milestones Grill (Toronto)
-            </h1>
-          </div>
-        </div>
-        {/* HEADER */} {/* DESCRIPTION PORTION */}
+        {/* NAVBAR */} 
+        {/* HEADER */}
+      <Header />
+        {/* HEADER */}
+         {/* DESCRIPTION PORTION */}
         <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
           <div className="bg-white w-[100%] rounded p-3 shadow">
             {/* RESTAURANT NAVBAR */}
-            <nav className="flex text-reg border-b pb-2">
-              <Link href="/restaurant/ninners" className="mr-7">
-                Overview
-              </Link>
-              <Link href="/restaurant/ninners/menu" className="mr-7">
-                Menu
-              </Link>
-            </nav>
+            <RestaurantNavBar />
             {/* RESTAURANT NAVBAR */} {/* MENU */}
-            <main className="bg-white mt-5">
-              <div>
-                <div className="mt-4 pb-1 mb-1">
-                  <h1 className="font-bold text-4xl">Menu</h1>
-                </div>
-                <div className="flex flex-wrap justify-between">
-                  {/* MENU CARD */}
-                  <div className=" border rounded p-3 w-[49%] mb-3">
-                    <h3 className="font-bold text-lg">Surf and Turf</h3>
-                    <p className="font-light mt-1 text-sm">
-                      A well done steak with lobster and rice
-                    </p>
-                    <p className="mt-7">$80.00</p>
-                  </div>
-                  {/* MENU CARD */}
-                </div>
-              </div>
-            </main>
+           <Menu />
             {/* MENU */}
           </div>
         </div>
